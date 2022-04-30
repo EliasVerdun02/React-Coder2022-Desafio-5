b25bcbcf0893b77952067a2fd445bde65b808134
Elias Verdun02 <eleliasds@gmail.com>
2022-04-30 14:35:40 -0300

    primer commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import './NavBar.css'
+
+function NavBar(){
+    return(
+        <nav className="nav">
+
+        <div className="container-logo">
+            <h1>TERRA INDUMENTARIA</h1>
+        </div>
+
+        <ul className="list">
+
+            <li className="list__item list__item-click">
+                    <a className="nav__link" href="#1">Marcas</a>
+            </li>
+            
+            
+            <li className="list__item">
+                    <a className="nav__link" href="#1">Hombre</a>
+            </li>
+
+            <li className="list__item list__item-click">
+                    <a className="nav__link" href="#1">Mujer</a>
+            </li>
+
+            <li className="list__item">
+                    <a className="nav__link" href="#1">Ofertas</a>
+            </li>
+
+        </ul>
+
+    </nav>
+    )
+}
+
+
+export default NavBar
